150a843f0d75351747bcd9ecdc9ab6e562155e5a
Daniel <itzdanielmichael@gmail.com>
2021-11-01 19:09:11 -0400

    update Main

--- dates/src/Main.java
+++ dates/src/Main.java
@@ -2,8 +2,19 @@ import java.util.Calendar;
 import java.util.TimeZone;
 
 public class Main {
-    public static void main(String[] args)  {
-      Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("EST"));
-      System.out.print("Day of Month: " + cal.get(Calendar.DAY_OF_MONTH));
-    }
+  public static void main(String[] args) {
+    Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("EST"));
+
+    printMyDateInfo(cal);
+  }
+    
+
+  /* https://www.dummies.com/programming/java/what-is-the-static-keyword-in-java/ */
+  private static void printMyDateInfo(Calendar cal) {
+    int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
+    int minute = cal.get(Calendar.MINUTE);
+    
+    System.out.print("Day of Month: " + dayOfMonth);
+    System.out.print("\nMinute: " + minute);
+  }
 }
